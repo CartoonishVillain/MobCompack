91e130ffcbbed64282920c53f4009f2dcd84c38f
Cartoonish Villain <jedijoe93@gmail.com>
2022-02-12 15:46:39 -0500

    Fix transparancy

--- src/main/java/com/cartoonishvillain/mobcompack/client/model/CrystallineSlimeLayer.java
+++ src/main/java/com/cartoonishvillain/mobcompack/client/model/CrystallineSlimeLayer.java
@@ -3,9 +3,12 @@ package com.cartoonishvillain.mobcompack.client.model;
 import com.cartoonishvillain.mobcompack.MobCompack;
 import com.cartoonishvillain.mobcompack.entity.bop.CrystallineSlime;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 
 public class CrystallineSlimeLayer extends RenderLayer<CrystallineSlime, CrystallineSlimeModel<CrystallineSlime>> {
@@ -17,6 +20,9 @@ public class CrystallineSlimeLayer extends RenderLayer<CrystallineSlime, Crystal
 
     @Override
     public void render(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, CrystallineSlime entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-        renderColoredCutoutModel(this.getParentModel(), TEXTURE, matrixStackIn, bufferIn, 200, entitylivingbaseIn, 1.0F, 1.0F, 1.0F);
+        if(!entitylivingbaseIn.isInvisible()){
+            VertexConsumer bb = bufferIn.getBuffer(RenderType.entityTranslucent(TEXTURE));
+            this.getParentModel().renderToBuffer(matrixStackIn, bb, packedLightIn, OverlayTexture.NO_OVERLAY, 1f, 1f, 1f, 1f);
+        }
     }
 }
